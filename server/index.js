7a8b9b40c635922d3bfc9fb42a4a47448aa37693
JW Garrison <jwolfgarrison@gmail.com>
2017-04-07 23:13:11 -0400

    Mount api router, change static path

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import path from 'path';
 
+import API_ROUTER from './routes/api';
+
 const PORT = 8000;
 const app = express();
 
@@ -15,12 +17,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, '../../public')));
+
+app.use('/api', API_ROUTER);
 
 app.listen(PORT, (err) => {
   if (err) {
     console.error('Cannot start server: ', err);
   } else {
-    console.log(`Listening to port : ${PORT})`;
+    console.log(`Listening to port : ${PORT}`);
   }
 });
