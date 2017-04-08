d246ce7e46d7a97bccf809914ce7e7ad7fd7d4cd
JW Garrison <jwolfgarrison@gmail.com>
2017-04-07 22:53:32 -0400

    Update server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
+import 'babel-polyfill';
 import express from 'express';
+import compression from 'compression';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import path from 'path';
 
+const PORT = 8000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
+app.use(compression());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8000, () => {
-  console.log('Listening on port 8000');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error('Cannot start server: ', err);
+  } else {
+    console.log(`Listening to port : ${PORT})`;
+  }
 });
